538a3f3648f167c1a91f11c24dc4ea881dc9687d
Alamod3 <eats@lottallamas.com>
2023-08-02 21:11:14 -0400

    messing around

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let name = '';
+</script>
+
+<h1>Welcome to PepeVote</h1>
+
+<input bind:value={name} placeholder="enter your name" />
+<p>Hello {name || 'stranger'}!</p>
