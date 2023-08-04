a3a714197fa728311721aa620f52dd14576b7998
Alamod3 <eats@lottallamas.com>
2023-08-03 22:28:15 -0400

    added generic server endpoint

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,23 @@
 <script>
-	let name = '';
+	export const random = '1234'
+	let wallet = '';
+	let message = '';
+	let token = '';
+	async function validate() {
+		const response = await fetch('/api/server', {
+			method: 'POST',
+			body: JSON.stringify({ token }),
+			headers: {
+				'content-type': 'application/json'
+			}
+		})
+
+		token = await response.json();
+	}
 </script>
 
 <h1>Welcome to PepeVote</h1>
-
-<input bind:value={name} placeholder="enter your name" />
-<p>Hello {name || 'stranger'}!</p>
+<p>Message: {random}</p>
+<input bind:value={wallet} placeholder="wallet" />
+<input bind:value={message} placeholder="signed message" />
+<button on:click={() => (validate())}>Submit</button>
