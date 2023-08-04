6a9a957b142a05b995ff63c5f3f7b20f48947790
Alamod3 <eats@lottallamas.com>
2023-08-04 14:28:35 -0400

    more general changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,62 @@
 <script>
-	export const random = '1234'
-	let wallet = '';
+	import { onMount } from 'svelte'
+	export let random = '';
+	// Replace with list of candidates
+	export const candidates = [
+		'14GRxZmNCLHo5Uknr2XYnGA61Hh9uMULXV',
+		'19h8nvZWqzpZnEufu611ZG6uZ5jYN1tytn',
+		'1FX2EMcKqKQbxPeVSWTdnBwiCp7ijpc9qp'
+	];
+
+	onMount(async () => {
+		random = self.crypto.randomUUID();
+	});
+
+	let address = '';
 	let message = '';
-	let token = '';
+	let vote = '';
+
 	async function validate() {
 		const response = await fetch('/api/server', {
 			method: 'POST',
-			body: JSON.stringify({ token }),
+			body: JSON.stringify({ address, message, random, vote }),
 			headers: {
 				'content-type': 'application/json'
 			}
 		})
+	}
 
-		token = await response.json();
+	function copyToClipboard() {
+		// @ts-ignore
+		const copy = document.getElementById('message').innerHTML;
+		navigator.clipboard.writeText(copy);
 	}
 </script>
 
 <h1>Welcome to PepeVote</h1>
-<p>Message: {random}</p>
-<input bind:value={wallet} placeholder="wallet" />
-<input bind:value={message} placeholder="signed message" />
-<button on:click={() => (validate())}>Submit</button>
+<p>Message: <span id="message">{random}</span><button on:click={() => (copyToClipboard())}>Copy</button></p>
+
+<form method="POST" on:submit|preventDefault={validate}>
+    <label>
+        Address
+        <input name="address" type="address" bind:value={address}>
+    </label>
+    <label>
+        Message
+        <input name="message" type="message" bind:value={message}>
+    </label>
+
+	{#each candidates as candidate}
+		<label>
+			<input
+				type="radio"
+				name="vote"
+				value={candidate}
+				bind:group={vote}
+			/>
+			{candidate}
+		</label>
+	{/each}
+    <button type="submit">Vote</button>
+</form>
+
--- src/routes/api/server/+server.ts
+++ src/routes/api/server/+server.ts
@@ -10,23 +10,25 @@ interface Asset {
 /** @type {import('./$types').RequestHandler} */
 export async function POST({ request }) {
 	try {
-		const { wallet, message, random } = await request.json();
-		const verified = new Message(random).verify(wallet, message);
+		const { address, message, random, vote } = await request.json();
+
+		const verified = new Message(random).verify(address, message);
 
 		if (!verified) {
 			throw error(400, 'Wallet could not be validated');
 		}
 
-		const response = await fetch(`https://xchain.io/api/balances/${wallet}`);
+		const response = await fetch(`https://xchain.io/api/balances/${address}`);
 		const { data } = await response.json();
 		const containsAsset = data.map((a: Asset) => a.asset).includes(ASSET);
 
 		if (!containsAsset) {
 			throw error(400, `Wallet does not contain ${ASSET}`);
 		}
-	} catch (e) {
-		console.log(e)
+		// Make post to Directus
+	} catch (e: any) {
+		throw error(e.status, e.body.message);
 	}
 
-    return new Response('HELLO')
+    return new Response()
 }
