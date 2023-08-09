a8f32416b0853fe92f9f7aca7d6771492ff4234f
Jeff Ruoss <jeffreyruoss@gmail.com>
2023-08-09 13:30:02 -0700

    How to vote link.

---
+++ src/lib/HowToVote.svelte
@@ -0,0 +1,29 @@
+<script>
+</script>
+
+<a href="https://rarepepes.com/vote/" target="_blank" class="how-to-vote">
+	<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>information-outline</title><path d="M11,9H13V7H11M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M11,17H13V11H11V17Z" /></svg>
+	<span>How to vote</span>
+</a>
+
+<style>
+	.how-to-vote {
+		margin-bottom: 15px;
+		color: #4B784E;
+		display: flex;
+		align-items: center;
+		border-bottom: 1px solid #4B784E;
+		max-width: 123px;
+		font-size: 0.9em;
+		&:hover {
+			color: #4e9e54;
+			text-decoration: none;
+			border-bottom: 1px solid #4e9e54;
+		}
+	}
+	.how-to-vote svg {
+		margin-right: 3px;
+		width: 19px;
+		height: 19px;
+	}
+</style>
