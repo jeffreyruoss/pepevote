f6e9346a145850b28f8233a69b9ec14003ab43a2
Jeff Ruoss <jeffreyruoss@gmail.com>
2023-08-10 16:31:56 -0700

    Add cookie bar.

---
+++ src/lib/CookieBar.svelte
@@ -0,0 +1,18 @@
+<script>
+</script>
+
+<div class="cookie-warning">
+	<div>
+		🍪 We use cookies only to keep you signed in. Please enable them for a seamless experience.
+	</div>
+</div>
+
+<style>
+	.cookie-warning {
+		padding: 1rem;
+		color: #fff;
+		font-size: 1rem;
+		text-align: center;
+		background-color: rgba(0, 54, 170, 0.9);
+	}
+</style>
